c94f7724a0fcbc7e61a4b86fb79367f9c898f94e
David MacMahon <davidm@astro.berkeley.edu>
2022-05-02 00:11:55 +0200

    Initial implementation

--- src/BluseRawWatch.jl
+++ src/BluseRawWatch.jl
@@ -1,5 +1,28 @@
 module BluseRawWatch
 
-# Write your package code here.
+export rediskey
+export dirwatcher_callback
+
+using Redis
+using INotify
+
+function rediskey()
+    hostname = split(gethostname(), '.')[end]
+    "bluse_raw_watch:$hostname"
+end
+
+function dirwatcher_callback(dir_event_name; redis, key)
+    dir, event, name = dir_event_name
+    # Ignore events for files that don't end with ".0000.raw"
+    endswith(name, ".0000.raw") || return
+    path = joinpath(dir, name)
+    if INotify.iscreate(event)
+        @info "adding $path to $key"
+        sadd(redis, key, path)
+    elseif INotify.isdelete(event)
+        @info "removing $path from $key"
+        srem(redis, key, path)
+    end
+end
 
 end
