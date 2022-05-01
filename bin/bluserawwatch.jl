006ab9b40400b2ee34e0af80580990e741928e75
David MacMahon <davidm@astro.berkeley.edu>
2022-05-02 00:16:14 +0200

    Add bin/bluserawwatch.jl script
    This executable script is intended to be run as a long running process.
    Typically it would be managed by process manager such as systemd,
    circusd, or monit.  The normal way to run this script is actually via
    bash rather than julia.  This allows determination of the project
    directory from the location of the script itself.  For this to work
    however, the script itself must be within the directory structure of
    this package and then symlinked to from a more "normal" spot (e.g.
    /usr/local/bin).  Because using Pkg.add() to add the package will put it
    in a tricky to find directory, it is currently recommended to Pkg.dev()
    this package.  This will put the script in the bin subdirectory of
    $HOME/.julia/dev/BlueRawWatch, making it easier to symlink to.

---
+++ bin/bluserawwatch.jl
@@ -0,0 +1,43 @@
+#!/bin/bash
+#=
+export JULIA_PROJECT=$(dirname $(dirname $(readlink -e "${BASH_SOURCE[0]}")))
+exec julia --color=yes --startup-file=no "${BASH_SOURCE[0]}" "$@"
+=#
+
+# This is primarily a script that used INotify.jl to watch for the creation and
+# deletion of `*.0000.raw` files in/under a given list of top level directories.
+#
+# When a new `*.0000.raw` file is created, it gets added a node-specifc redis
+# set that is indexed as "bluse_raw_watch:$node".  The value added to the set is a
+# string containing the full pathname of the added file.
+#
+# When a `*.0000.raw` file is deleted, the full pathname of the deleted file
+# will be removed from the Redis set.
+
+using BluseRawWatch
+using Redis
+using INotify
+
+function main(dir)
+    redishost = get(ENV, "REDISHOST", "redishost")
+    redis = Redis.RedisConnection(host=redishost)
+    key = rediskey()
+
+    @info "watching $dir"
+    dw = INotify.DirWatcher(dirwatcher_callback, dir, INotify.CREATE|INotify.DELETE; redis=redis, key=key)
+
+    Base.exit_on_sigint(false)
+    try
+        wait(dw.task[])
+    catch e
+        e isa InterruptException || rethrow()
+    finally
+        close(dw)
+    end
+end
+
+if isempty(ARGS)
+    println("usage: $(basename(PROGRAM_FILE)) DIR")
+else
+    main(ARGS[1])
+end
