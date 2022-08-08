92fce4fe57b20c04be815eb4e2a5914479a7dd43
David MacMahon <davidm@astro.berkeley.edu>
2022-08-09 01:34:50 +0200

    Log exceptions in dirwatcher_callback

--- src/BluseRawWatch.jl
+++ src/BluseRawWatch.jl
@@ -15,17 +15,24 @@ function rediskey()
 end
 
 function dirwatcher_callback(dir_event_name; redis, key)
-    dir, event, name = dir_event_name
-    # Ignore events for files that don't end with ".0000.raw"
-    endswith(name, ".0000.raw") || return
-    path = joinpath(dir, name)
-    if INotify.iscreate(event)
-        @info "adding $path to $key"
-        sadd(redis, key, path)
-    elseif INotify.isdelete(event)
-        @info "removing $path from $key"
-        srem(redis, key, path)
+    try
+        dir, event, name = dir_event_name
+        # Ignore events for files that don't end with ".0000.raw"
+        endswith(name, ".0000.raw") || return
+        path = joinpath(dir, name)
+        if INotify.iscreate(event)
+            @info "adding $path to $key"
+            sadd(redis, key, path)
+        elseif INotify.isdelete(event)
+            @info "removing $path from $key"
+            srem(redis, key, path)
+        end
+    catch
+        for (exc, bt) in Base.catch_stack()
+            showerror(stderr, exc, bt)
+            println(stderr)
+        end
     end
 end
 
-end
+end # module BluseRawWatch
